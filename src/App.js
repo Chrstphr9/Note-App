262619a34e7a3b582d3ca0f4f7ae4c3ac060e6b3
Chstphr <somto565@gmail.com>
2023-01-24 16:32:06 +0100

    routed th pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom"
+import CreateNote from "./pages/CreateNote";
+import Notes from "./pages/Notes";
+import EditNote from "./pages/EditNote";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Notes />} />
+        <Route path="/create-note" element={<CreateNote />} />
+        <Route path="/edit-note/:id" element={<EditNote />} />
+      </Routes>
+    </Router>
   );
 }
 
