70f65f14a85e98d7d08774cbad287070e8c8d685
Chstphr <somto565@gmail.com>
2023-01-25 08:16:47 +0100

    formatted notes document

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -1,30 +1,32 @@
-import React from 'react'
-import dummyNotes from '../dummy_notes'
-import {
-  CiSearch
-} from 'react-icons/ci'
-import {
-  BsPlusLg 
-} from 'react-icons/bs'
-import { Link } from 'react-router-dom'
-
+import React from "react";
+import dummyNotes from "../dummy_notes";
+import { CiSearch } from "react-icons/ci";
+import { BsPlusLg } from "react-icons/bs";
+import { Link } from "react-router-dom";
+import NoteItem from "../components/NoteItem";
 
 const Notes = () => {
   return (
     <section>
-      <header className='notes__header'>
-          <h2> My Notes </h2>
-          {/* <input type="text" autoFocus placeholder='Keywords....' /> */}
-          <button className='btn'> <CiSearch /> </button>
+      <header className="notes__header">
+        <h2> My Notes </h2>
+        {/* <input type="text" autoFocus placeholder='Keywords....' /> */}
+        <button className="btn">
+          {" "}
+          <CiSearch />{" "}
+        </button>
       </header>
-      <div className='note__container'>
-        {
-            dummyNotes.map(note => <NoteItem key={note.id } />)
-        }
+      <div className="note__container">
+        {dummyNotes.map((note) => (
+          <NoteItem key={note.id} />
+        ))}
       </div>
-      <Link className='btn add__btn'> <BsPlusLg /> </Link>
+      <Link className="btn add__btn">
+        {" "}
+        <BsPlusLg />{" "}
+      </Link>
     </section>
-  )
-}
+  );
+};
 
-export default Notes
+export default Notes;
