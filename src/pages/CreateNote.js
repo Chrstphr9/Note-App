f18c5e8d159c9a9397b72c737d7e52d456edff0c
Chstphr <somto565@gmail.com>
2023-01-26 15:27:31 +0100

    passing props

--- src/pages/CreateNote.js
+++ src/pages/CreateNote.js
@@ -1,20 +1,26 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { IoIosArrowBack } from 'react-icons/io';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { IoIosArrowBack } from "react-icons/io";
 
 const CreateNote = () => {
+  const [title, setTitle] = useState("");
+  const [details, setDetails] = useState("");
+
   return (
     <section>
-      <header className='create-note__header'>
-          <Link to='/' className='btn'> <IoIosArrowBack/> </Link>
-          <button className='btn lg primary'>Save</button>
+      <header className="create-note__header">
+        <Link to="/" className="btn">
+          {" "}
+          <IoIosArrowBack />{" "}
+        </Link>
+        <button className="btn lg primary">Save</button>
       </header>
-      <form className='create-note__form'>
-        <input type='text' placeholder='Title' autoFocus />
-        <textarea rows='28' placeholder='Note Details....'></textarea>
+      <form className="create-note__form">
+        <input type="text" placeholder="Title" value={title}  onChange={(e) => setTitle(e.target.value)} autoFocus />
+        <textarea rows="28" placeholder="Note Details...." value={details}  onChange={(e) => setDetails(e.target.value)}></textarea>
       </form>
     </section>
-  )
-}
+  );
+};
 
-export default CreateNote
+export default CreateNote;
