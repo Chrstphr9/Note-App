e14c3ab2629283839fd94c1a16d4363aa30c39a6
Chstphr <somto565@gmail.com>
2023-01-29 13:52:14 +0100

    resolved an error in the useCreateDate

--- src/components/useCreateDate.jsx
+++ src/components/useCreateDate.jsx
@@ -41,7 +41,7 @@ const useCreateDate = () => {
       break;
   }
 
-  const date = `${monthName} ${dateObj.getDate()}, ${dateObj.getFullYear()} [$ {dateObj.getHours()}:${dateObj.getMinutes()}:${dateObj.getSecond()}]`;
+  const date = `${monthName} ${dateObj.getDate()}, ${dateObj.getFullYear()} [${dateObj.getHours()}:${dateObj.getMinutes()}}]`;
 return date;  
 };
 
